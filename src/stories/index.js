5fd38b636f99a51933f5f26610f66edd3cfccb36
eatsjobs <pasquale.mangialavori@gmail.com>
2017-11-11 23:47:32 +0100

    Storybook added

---
+++ src/stories/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+import Card from '../components/Card/Card.jsx';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('Card', module)
+  .add('standard', () => {
+    return (<Card flipped={false} matched={true} index={0} imageSrc='http://via.placeholder.com/265x265' type={'test'} />)
+  })
