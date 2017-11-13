056e7f9faeb4d7857cb53bb398d1c6b0dc33e0db
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2017-11-13 09:32:35 +0100

    Gamestatus minimal css

--- src/components/GameStatus/GameStatus.jsx
+++ src/components/GameStatus/GameStatus.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './GameStatus.css';
 export default function GameStatus(props) {
     return (
-        <div style={{ width: '100%' }}> 
-            <div>Moves: {props.moves}</div>
+        <div className={'GameStatus-container'}> 
+            <div className={'GameStatus-moves'}>Moves: {props.moves}</div>
             <button onClick={props.onButtonRestart}>Restart</button>
             <div>{props.hasWon ? "You Won!" : null }</div>
         </div>
